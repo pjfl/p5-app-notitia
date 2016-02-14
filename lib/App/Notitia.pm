e56dc8ca5fe69ea98292286d7aa9aa90307bf053
pjfl <github@roxsoft.co.uk>
2016-02-14 00:47:20 +0000

    Initialised by Module::Provision

---
+++ lib/App/Notitia.pm
@@ -0,0 +1,87 @@
+package App::Notitia;
+
+use 5.010001;
+use namespace::autoclean;
+use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 1 $ =~ /\d+/gmx );
+
+use Class::Usul::Constants;
+use Class::Usul::Functions  qw( throw );
+use Moo;
+
+extends q(Class::Usul::Programs);
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 Name
+
+App::Notitia - One-line description of the modules purpose
+
+=head1 Synopsis
+
+   use App::Notitia;
+   # Brief but working code examples
+
+=head1 Description
+
+=head1 Configuration and Environment
+
+Defines the following attributes;
+
+=over 3
+
+=back
+
+=head1 Subroutines/Methods
+
+=head1 Diagnostics
+
+=head1 Dependencies
+
+=over 3
+
+=item L<Class::Usul>
+
+=back
+
+=head1 Incompatibilities
+
+There are no known incompatibilities in this module
+
+=head1 Bugs and Limitations
+
+There are no known bugs in this module. Please report problems to
+http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Notitia.
+Patches are welcome
+
+=head1 Acknowledgements
+
+Larry Wall - For the Perl programming language
+
+=head1 Author
+
+Peter Flanigan, C<< <pjfl@cpan.org> >>
+
+=head1 License and Copyright
+
+Copyright (c) 2016 Peter Flanigan. All rights reserved
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself. See L<perlartistic>
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+
+=cut
+
+# Local Variables:
+# mode: perl
+# tab-width: 3
+# End:
+# vim: expandtab shiftwidth=3:
---
+++ t/10test_script.t
@@ -0,0 +1,13 @@
+use t::boilerplate;
+
+use Test::More;
+
+use_ok 'App::Notitia';
+
+done_testing;
+
+# Local Variables:
+# mode: perl
+# tab-width: 3
+# End:
+# vim: expandtab shiftwidth=3:
