f3dd4a221e946253b50f29e1ac06e1b5af146ebc
pjfl <github@roxsoft.co.uk>
2016-04-02 14:09:38 +0100

    Fixed baseline in checkbox replacement images

--- lib/App/Notitia.pm
+++ lib/App/Notitia.pm
@@ -2,7 +2,7 @@ package App::Notitia;
 
 use 5.010001;
 use strictures;
-use version; our $VERSION = qv( sprintf '0.3.%d', q$Rev: 27 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.3.%d', q$Rev: 28 $ =~ /\d+/gmx );
 
 use Class::Usul::Functions  qw( ns_environment );
 
@@ -32,7 +32,7 @@ App::Notitia - People and resource scheduling
 
 =head1 Version
 
-This documents version v0.3.$Rev: 27 $ of B<App::Notitia>
+This documents version v0.3.$Rev: 28 $ of B<App::Notitia>
 
 =head1 Description
 
