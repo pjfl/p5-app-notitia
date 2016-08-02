b9ad46cbe41a35f0f86d4459f11a142412a9a4b3
pjfl <github@roxsoft.co.uk>
2016-08-02 16:43:33 +0100

    Refactored month rota

--- lib/App/Notitia.pm
+++ lib/App/Notitia.pm
@@ -2,7 +2,7 @@ package App::Notitia;
 
 use 5.010001;
 use strictures;
-use version; our $VERSION = qv( sprintf '0.3.%d', q$Rev: 248 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.3.%d', q$Rev: 249 $ =~ /\d+/gmx );
 
 use Class::Usul::Functions  qw( ns_environment );
 
@@ -42,7 +42,7 @@ App::Notitia - People and resource scheduling
 
 =head1 Version
 
-This documents version v0.3.$Rev: 248 $ of B<App::Notitia>
+This documents version v0.3.$Rev: 249 $ of B<App::Notitia>
 
 =head1 Description
 
