d66ce98b546d8c732f7a5a7cd1f362b14d8f1211
pjfl <github@roxsoft.co.uk>
2016-11-18 00:34:53 +0000

    Bumps Web::ComposableRequest

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ App::Notitia - People and resource scheduling
 
 # Version
 
-This documents version v0.9.$Rev: 54 $ of **App::Notitia**
+This documents version v0.10.$Rev: 1 $ of **App::Notitia**
 
 # Description
 
--- lib/App/Notitia.pm
+++ lib/App/Notitia.pm
@@ -2,7 +2,7 @@ package App::Notitia;
 
 use 5.010001;
 use strictures;
-use version; our $VERSION = qv( sprintf '0.9.%d', q$Rev: 56 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.10.%d', q$Rev: 1 $ =~ /\d+/gmx );
 
 use Class::Usul::Functions  qw( ns_environment );
 
@@ -12,7 +12,7 @@ sub env_var {
 
 # TODO: Automate incrementing of schema versions
 sub schema_version {
-   return qv( '0.9.44' );
+   return qv( '0.10.0' );
 }
 
 1;
@@ -43,7 +43,7 @@ App::Notitia - People and resource scheduling
 
 =head1 Version
 
-This documents version v0.9.$Rev: 56 $ of B<App::Notitia>
+This documents version v0.10.$Rev: 1 $ of B<App::Notitia>
 
 =head1 Description
 
