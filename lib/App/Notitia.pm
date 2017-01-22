adbe94c8910839eb1f046473bee1af0d821e0be3
pjfl <github@roxsoft.co.uk>
2017-01-22 12:29:33 +0000

    Pod spelling

--- lib/App/Notitia.pm
+++ lib/App/Notitia.pm
@@ -2,7 +2,7 @@ package App::Notitia;
 
 use 5.010001;
 use strictures;
-use version; our $VERSION = qv( sprintf '0.11.%d', q$Rev: 12 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.11.%d', q$Rev: 13 $ =~ /\d+/gmx );
 
 use Class::Usul::Functions  qw( ns_environment );
 
@@ -43,7 +43,7 @@ App::Notitia - People and resource scheduling
 
 =head1 Version
 
-This documents version v0.11.$Rev: 12 $ of B<App::Notitia>
+This documents version v0.11.$Rev: 13 $ of B<App::Notitia>
 
 =head1 Description
 
@@ -227,7 +227,7 @@ Help for command line options can be found be running:
    bin/notitia-cli list-methods
    bin/notitia-cli help <method>
 
-The C<list-methods> command is available to all of the appliction programs
+The C<list-methods> command is available to all of the application programs
 (except scripts and C<notitia-server>)
 
 The bash completion script is provided by:
