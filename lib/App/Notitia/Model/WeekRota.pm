4115f3ab5594f6c2bc4ee41636fbf2ba296fd595
John Sargent <github@j-bg.co.uk>
2017-03-06 19:19:24 +0000

    Fudge factor for more realistic distances

--- lib/App/Notitia/Model/WeekRota.pm
+++ lib/App/Notitia/Model/WeekRota.pm
@@ -192,6 +192,13 @@ my $_find_location = sub {
    return $list->[ $i ] ? $list->[ $i ]->{location} : FALSE;
 };
 
+my $_reality_adjustment = sub {
+   my ($distance, $factor) = @_;
+
+   return ($distance/$factor) + sqrt( ($distance**2) - ($distance/$factor)**2 );
+};
+
+
 my $_calculate_distance = sub { # In metres
    my ($location, $assignee) = @_;
 
@@ -200,8 +207,9 @@ my $_calculate_distance = sub { # In metres
 
    my ($lx, $ly) = split m{ , }mx, $location->coordinates;
    my ($ax, $ay) = split m{ , }mx, $assignee->coordinates;
+   my $distance = int 0.5 + sqrt( ($lx - $ax)**2 + ($ly - $ay)**2 );
 
-   return int 0.5 + sqrt( ($lx - $ax)**2 + ($ly - $ay)**2 );
+   return $_reality_adjustment->( $distance, 5 );
 };
 
 my $_vehicle_label = sub {
